16e4011c463e5e0165882f92737831c27fc4e8de
shivam singh <shivamsingh29626@gmail.com>
2023-04-14 17:25:06 +0530

    initial commit

---
+++ src/component/Footer.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+export default class Footer extends Component {
+  render() {
+    return (
+      <div className='container'>
+       <footer className='fixed-bottom'>
+        <p className='text-muted'>© Shivam Singh 2023</p>
+       </footer>
+      </div>
+    )
+  }
+}
